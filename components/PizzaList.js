aa18229da1c4295804e07a990e5c66b8b0e7ab34
elcinp <elcinpars1@gmail.com>
2022-03-06 15:05:42 +0300

    PizzaList component created

---
+++ components/PizzaList.js
@@ -0,0 +1,11 @@
+import styles from '../styles/PizzaList.module.css'
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default PizzaList

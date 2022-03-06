33741dff3cf6ec3c237caf755804f2580ef3cbdb
elcinp <elcinpars1@gmail.com>
2022-03-06 14:53:28 +0300

    handleArrow function added

--- components/Featured.js
+++ components/Featured.js
@@ -1,28 +1,49 @@
 import styles from "../styles/Featured.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 const Featured = () => {
   const images = [
-    "/img/featured.png",
-    "/img/featured2.png",
-    "/img/featured3.png",
+    "/img/featured.jpg",
+    "/img/featured2.jpg",
+    "/img/featured3.jpg",
   ];
+
+  const [index, setIndex] = useState(0);
+
+  const handleArrow = (direction) => {
+    if (direction === "l") {
+      setIndex(index !== 0 ? index - 1 : 2);
+    }
+    if (direction === "r") {
+      setIndex(index !== 2 ? index + 1 : 0);
+    }
+  };
+  console.log(index);
   return (
     <div className={styles.container}>
-      <Image src="/img/arrowl.png" alt="" width="32" height="32" />
-      <div className={styles.wrapper}>
-        <div className={styles.imgContainer}>
-          {images.map((img, i) => (
-            <Image
-              src="/img/featured.png"
-              key={i}
-              alt=""
-              layout="fill"
-            />
-          ))}
-        </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
+        <Image src="/img/arrowl.png" layout="fill" alt="" objectFit="contain" />
       </div>
-      <Image src="/img/arrowr.png" alt="" width="32" height="32" />
+
+      <div className={styles.wrapper}>
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
+        <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain" />
+      </div>
     </div>
   );
 };

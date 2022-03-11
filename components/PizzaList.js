8253c3c25b351a00a018d575f07b53f5e955a29a
elcinp <elcinpars1@gmail.com>
2022-03-11 16:32:47 +0300

    PizzaList component added

--- components/PizzaList.js
+++ components/PizzaList.js
@@ -1,9 +1,20 @@
 import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
 
 const PizzaList = () => {
   return (
     <div className={styles.container}>
-        
+        <h1 className={styles.title}>The Best PIZZA IN TOWN</h1>
+        <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, culpa sint deleniti accusamus qui veniam libero, ea, ab natus eius deserunt enim eum iusto cum! Velit, accusantium? Laboriosam, pariatur distinctio.</p>
+        <div className={styles.wrapper}>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+        </div>
     </div>
   )
 }
